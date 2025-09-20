01555bb4db4d5402f393fb1ac9dda1f05a3ac2a6
IsaiahDakin <IsaiahDak16@gmail.com>
2025-09-19 21:36:05 -0300

    Visual & Layout Changes.
    + hamburger menu
    + new color scheme
    + logo
    + some other stuff..

---
+++ src/scripts/ham-menu.js
@@ -0,0 +1,8 @@
+const hamMenu = document.querySelector('.ham-menu');
+
+const offScreenMenu = document.querySelector('.off-screen-menu');
+
+hamMenu.addEventListener('click', () => {
+    offScreenMenu.classList.toggle('active');
+    hamMenu.classList.toggle('active');
+})
